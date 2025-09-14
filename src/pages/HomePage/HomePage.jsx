d66a07234da35ee2bf2b0b9bbf571b79f048265d
Oleksiy <Unialex@ukr.net>
2025-09-14 23:27:10 +0300

    little styles fix

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,14 +3,16 @@ import { Link } from 'react-router-dom';
 
 export default function HomePage() {
   return (
-    <div className={css.wrapper}>
-      <h1 className={css.title}>Find your perfect rental car</h1>
-      <p className={css.text}>
-        Reliable and budget-friendly rentals for any journey
-      </p>
-      <Link to="/catalog" className={css.button}>
-        View Catalog
-      </Link>
-    </div>
+    <section className={css.wrapper}>
+      <div>
+        <h1 className={css.title}>Find your perfect rental car</h1>
+        <p className={css.text}>
+          Reliable and budget-friendly rentals for any journey
+        </p>
+        <Link to="/catalog" className={css.button}>
+          View Catalog
+        </Link>
+      </div>
+    </section>
   );
 }
