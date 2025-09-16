a248e530bff89a5376ac22ba4e4c6f97bb85ac4e
Oleksiy <Unialex@ukr.net>
2025-09-17 00:44:58 +0300

    move api logic to separate file

---
+++ src/api/api.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+axios.defaults.baseURL = 'https://car-rental-api.goit.global';
+
+export async function fetchCars(page = 1, limit = 12) {
+  const axiosParams = { page, limit };
+  const response = await axios.get('/cars', { params: axiosParams });
+
+  const paginationParams = calculatePaginationData(
+    Number(response.data.totalCars),
+    Number(response.data.page),
+    Number(response.data.totalPages),
+  );
+
+  return { cars: response.data.cars, paginationParams };
+}
+
+export async function fetchCarById(id) {
+  const response = await axios.get(`/cars/${id}`);
+  return response.data;
+}
+
+function calculatePaginationData(count, page, totalPages) {
+  const hasNextPage = totalPages > page;
+  const hasPreviousPage = page !== 1;
+
+  return {
+    totalCars: count,
+    page,
+    totalPages,
+    hasNextPage,
+    hasPreviousPage,
+  };
+}
--- src/redux/cars/carsOperations.js
+++ src/redux/cars/carsOperations.js
@@ -1,39 +1,15 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
-
-axios.defaults.baseURL = 'https://car-rental-api.goit.global';
+import { fetchCars } from '../../api/api.js';
 
 export const getCars = createAsyncThunk(
   'cars/getAll',
   async (params = {}, thunkAPI) => {
     const { page = 1, limit = 12 } = params;
 
-    const axiosParams = { page, limit };
     try {
-      const response = await axios.get('/cars', { params: axiosParams });
-
-      const paginationParams = calculatePaginationData(
-        Number(response.data.totalCars),
-        Number(response.data.page),
-        Number(response.data.totalPages),
-      );
-
-      return { cars: response.data.cars, paginationParams };
+      return await fetchCars(page, limit);
     } catch (err) {
       return thunkAPI.rejectWithValue(err.message);
     }
   },
 );
-
-function calculatePaginationData(count, page, totalPages) {
-  const hasNextPage = totalPages > page;
-  const hasPreviousPage = page !== 1;
-
-  return {
-    totalCars: count,
-    page,
-    totalPages,
-    hasNextPage,
-    hasPreviousPage,
-  };
-}
