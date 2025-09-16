6b4746a70c0f7b0fcce4adb5842386c127a5e5d0
Oleksiy <Unialex@ukr.net>
2025-09-17 00:35:13 +0300

    add container comp.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,17 @@
 import css from './Header.module.css';
 import Logo from '../Logo/Logo.jsx';
 import MainNav from '../MainNav/MainNav.jsx';
+import Container from '../Container/Container.jsx';
 
 export default function Header() {
   return (
-    <header className={css.wrapper}>
-      <Logo />
-      <MainNav />
+    <header className={css.header}>
+      <Container>
+        <div className={css.wrapper}>
+          <Logo />
+          <MainNav />
+        </div>
+      </Container>
     </header>
   );
 }
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,11 @@
+import Container from '../../components/Container/Container.jsx';
 import css from './HomePage.module.css';
 import { Link } from 'react-router-dom';
 
 export default function HomePage() {
   return (
-    <section className={css.wrapper}>
-      <div>
+    <section className={css.section}>
+      <Container>
         <h1 className={css.title}>Find your perfect rental car</h1>
         <p className={css.text}>
           Reliable and budget-friendly rentals for any journey
@@ -12,7 +13,7 @@ export default function HomePage() {
         <Link to="/catalog" className={css.button}>
           View Catalog
         </Link>
-      </div>
+      </Container>
     </section>
   );
 }
