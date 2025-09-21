3edd7e0700e890bd5a5f8473968e75fec819ff64
Oleksiy <Unialex@ukr.net>
2025-09-21 15:50:59 +0300

    add persist store for favorites

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,13 +2,40 @@ import { configureStore } from '@reduxjs/toolkit';
 import carsReducer from './cars/carsSlice.js';
 import filtersReducer from './filters/filtersSlice.js';
 import favoritesReducer from './favorites/favoritesSlice.js';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistedFavoritesReducer = persistReducer(
+  {
+    key: 'favorites',
+    storage,
+    whitelist: ['favorites'],
+  },
+  favoritesReducer,
+);
 
 const store = configureStore({
   reducer: {
     cars: carsReducer,
     filters: filtersReducer,
-    favorites: favoritesReducer,
+    favorites: persistedFavoritesReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
+export const persistor = persistStore(store);
 export default store;
